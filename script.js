c0c7b9f45d44369d94f13df0dd448a216ec194b9
siphesihle-gonya <gonya.siphesihle1@gmail.com>
2025-06-04 02:24:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,409 @@
-var strokesLeftBottom = $('#LeftBottomGroup_1_ path[id^=Stroke]').toArray().reverse();
-var strokesLeftTop = $('#LeftTopGroup_1_ path[id^=Stroke]').toArray().reverse();
-var strokesRightBottom = $('#RightBottomGroup_1_ path[id^=Stroke]').toArray().reverse();
-var strokesRightTop = $('#RightTopGroup_1_ path[id^=Stroke]').toArray().reverse();
+const qs = document.querySelector.bind(document);
+const easingHeart = mojs.easing.path(
+"M0,100C2.9,86.7,33.6-7.3,46-7.3s15.2,22.7,26,22.7S89,0,100,0");
 
-var stemGroup1 = ['#Stem2_1_', '#Stem3_1_', '#Stem4_1_', '#Stem5a_1_', '#Stem5b_1_', '#Stem6_1_', '#Stem7a_1_', '#Stem7b_1_', '#Stem7c_1_', '#Stem8_1_'];
-var stemGroup2 = ['#Stem17_1_', '#Stem18_1_', '#Stem19_1_', '#Stem20a_1_', '#Stem20b_1_', '#Stem21_1_', '#Stem22a_1_', '#Stem22b_1_', '#Stem22c_1_', '#Stem23_1_'];
-var stemGroup3 = ['#Stem10_1_', '#Stem11_1_', '#Stem12a_1_', '#Stem12b_1_'];
-var stemGroup4 = ['#Stem26_1_', '#Stem27_1_', '#Stem28a_1_', '#Stem28b_1_'];
-var stemGroup5 = ['#Stem13a_1_', '#Stem13b_1_', '#Stem13c_1_'];
-var stemGroup6 = ['#Stem29a_1_', '#Stem29b_1_', '#Stem29c_1_'];
-var stemVarsFrom = { drawSVG: "0% 0%", autoAlpha: 1 };
-var stemVarsTo = { drawSVG: "0% 100%", duration:2, stagger:0.5 };
 
-var leafGroup1 = ['#Leaf2_1_','#Leaf17_1_'];
-var leafGroup2 = ['#Leaf4_1_','#Leaf5a_1_', '#Leaf5b_1_'];
-var leafGroup3 = ['#Leaf19_1_','#Leaf20a_1_', '#Leaf20b_1_'];
-var leafGroup4 = ['#Leaf11_1_','#Leaf12a_1_', '#Leaf12b_1_'];
-var leafGroup5 = ['#Leaf27_1_','#Leaf28a_1_', '#Leaf28b_1_'];
-var leafGroup6 = ['#Leaf13a_1_','#Leaf13b_1_', '#Leaf13c_1_'];
-var leafGroup7 = ['#Leaf29a_1_','#Leaf29b_1_', '#Leaf29c_1_'];
+const el = {
+  container: qs(".mo-container"),
 
-var budGroup1 = ['#Bud3_1_', '#Bud6_1_'];
-var budGroup2 = ['#Bud18_1_', '#Bud21_1_'];
-var budGroup3 = ['#Bud7a_1_', '#Bud7b_1_', '#Bud7c_1_', '#Bud8_1_'];
-var budGroup4 = ['#Bud22a_1_', '#Bud22b_1_', '#Bud22c_1_', '#Bud23_1_'];
-var budGroup5 = ['#Bud10_1_', '#Bud26_1_'];
+  i: qs(".lttr--I"),
+  l: qs(".lttr--L"),
+  o: qs(".lttr--O"),
+  v: qs(".lttr--V"),
+  e: qs(".lttr--E"),
+  y: qs(".lttr--Y"),
+  o2: qs(".lttr--O2"),
+  u: qs(".lttr--U"),
 
-var dots = $('#Dots_1_');
+  lineLeft: qs(".line--left"),
+  lineRight: qs(".line--rght"),
 
-var tl = gsap.timeline()
-.set('#Footer_group_1_', {autoAlpha: 1})
-.fromTo( ['#Stem16_1_','#Stem1_1_'], {drawSVG: "0% 0%" }, {duration:1.5, drawSVG: "0% 100%" }, 'start')
-.fromTo('#BaseGroup16_1_ path', {autoAlpha: 1, scale: 0, transformOrigin: '-10% 130%'}, {duration:1, scale:1}, 1.2, 'flower1')
-.fromTo('#PinkFlowerGroup16_1_', {autoAlpha: 1, scale: 0, transformOrigin: '35% 110%'}, {duration:2, scale:1}, 'flower1-=0.55')
-.fromTo('#BaseGroup1_1_ path', {autoAlpha: 1, scale: 0, transformOrigin: '90% 130%'}, {duration:1, scale:1}, 1.2, 'flower1')
-.fromTo('#PinkFlowerGroup1_1_', {autoAlpha: 1, scale: 0, transformOrigin: '65% 110%'}, {duration:2, scale:1}, 'flower1-=0.55')
-.fromTo( ['#Stem9_1_','#Stem25_1_'], {drawSVG: "0% 0%",  autoAlpha: 1 }, {duration:2, drawSVG: "0% 100%" }, 'flower1+=0.5')
-.fromTo('#BaseGroup9_1_ path', {autoAlpha: 1, scale: 0, transformOrigin: '-10% 130%'}, {duration:1, scale:1}, 'flower2-=0.5')
-.fromTo('#PinkFlowerGroup9_1_', {autoAlpha: 1, scale: 0, transformOrigin: '5% 110%'}, {duration:2, scale:1}, 'flower2')
-.fromTo('#BaseGroup25_1_ path', {autoAlpha: 1, scale: 0, transformOrigin: '105% 130%'}, {duration:1, scale:1}, 'flower2-=0.5')
-.fromTo('#PinkFlowerGroup25_1_', {autoAlpha: 1, scale: 0, transformOrigin: '95% 110%'}, {duration:2, scale:1}, 'flower2')
+  colTxt: "#763c8c",
+  colHeart: "#fa4843",
 
-//stems
-.fromTo(stemGroup1, stemVarsFrom, stemVarsTo, 'start+=0.1')
-.fromTo(stemGroup2, stemVarsFrom, stemVarsTo, 'start+=0.1')
-.fromTo(stemGroup3, stemVarsFrom, stemVarsTo, 'flower2-=1.5')
-.fromTo(stemGroup4, stemVarsFrom, stemVarsTo, 'flower2-=1.5')
-.fromTo(stemGroup5, stemVarsFrom, stemVarsTo, 'flower3-=1')
-.fromTo(stemGroup6, stemVarsFrom, stemVarsTo, 'flower3-=1')
+  blup: qs(".blup"),
+  blop: qs(".blop"),
+  sound: qs(".sound") };
 
-//leaves
-.fromTo(leafGroup1, {autoAlpha: 1, scale: 0, transformOrigin:gsap.utils.wrap(["80% 45%", "20% 45%"]) }, {duration:2, scale:1}, 'flower1-=1')
-.fromTo(leafGroup2, {autoAlpha: 1, scale: 0, transformOrigin:gsap.utils.wrap(["80% 25%", "60% 35%", "70% 75%"]) }, {duration:2, stagger:0.5, scale:1}, 'flower1')
-.fromTo(leafGroup3, {autoAlpha: 1, scale: 0, transformOrigin:gsap.utils.wrap(["20% 25%", "40% 35%", "30% 75%"]) }, {duration:2, stagger:0.5, scale:1}, 'flower1')
-.fromTo(leafGroup4, {autoAlpha: 1, scale: 0, transformOrigin:gsap.utils.wrap(["90% 70%", "100% 100%", "0% 90%"]) }, {duration:2, stagger:0.5, scale:1}, 'flower2')
-.fromTo(leafGroup5, {autoAlpha: 1, scale: 0, transformOrigin:gsap.utils.wrap(["10% 70%", "0% 100%", "100% 90%"]) }, {duration:2, stagger:0.5, scale:1}, 'flower2')
-.fromTo(leafGroup6, {autoAlpha: 1, scale: 0, transformOrigin:gsap.utils.wrap(["0% 90%", "10% 50%", "70% 60%"]) }, {duration:2, stagger:0.5, scale:1}, 'flower3')
-.fromTo(leafGroup7, {autoAlpha: 1, scale: 0, transformOrigin:gsap.utils.wrap(["100% 90%", "90% 50%", "30% 60%"]) }, {duration:2, stagger:0.5, scale:1}, 'flower3')
 
-//buds
-.fromTo(budGroup1, {autoAlpha: 1, scale: 0, transformOrigin:gsap.utils.wrap(['220% -10%', '55% 100%']) }, {duration:2, scale:1, stagger:2.75}, 'flower1-=0.75')
-.fromTo(budGroup2, {autoAlpha: 1, scale: 0, transformOrigin:gsap.utils.wrap(['-120% -10%', '45% 100%']) }, {duration:2, scale:1, stagger:2.75}, 'flower1-=0.75')
-.fromTo(budGroup3, {autoAlpha: 1, scale: 0, transformOrigin:gsap.utils.wrap(['10% 110%', '0% 100%', '0% 100%', '80% 100%']) }, {duration:2, scale:1, stagger:0.5},  'flower2')
-.fromTo(budGroup4, {autoAlpha: 1, scale: 0, transformOrigin:gsap.utils.wrap(['90% 110%', '100% 100%', '100% 100%', '20% 100%']) }, {duration:2, scale:1, stagger:0.5}, 'flower2')
-.fromTo(budGroup5, {autoAlpha: 1, scale: 0, transformOrigin:gsap.utils.wrap(['-50% 120%', '150% 120%']) }, {duration:2, scale:1},  'flower2-=0.5')
+class Heart extends mojs.CustomShape {
+  getShape() {
+    return '<path d="M50,88.9C25.5,78.2,0.5,54.4,3.8,31.1S41.3,1.8,50,29.9c8.7-28.2,42.8-22.2,46.2,1.2S74.5,78.2,50,88.9z"/>';
+  }
+  getLength() {
+    return 200;
+  }}
 
-//strokes
-.fromTo(strokesLeftBottom, stemVarsFrom, { drawSVG: "0% 100%", duration:2, stagger:1 }, 1)
-.fromTo(strokesRightBottom, stemVarsFrom, { drawSVG: "0% 100%", duration:2, stagger:1 }, 1)
-.fromTo(strokesLeftTop, stemVarsFrom, { drawSVG: "0% 100%", duration:2, stagger:1 }, 'flower1+=0.5')
-.fromTo(strokesRightTop, stemVarsFrom, { drawSVG: "0% 100%", duration:2, stagger:1 }, 'flower1+=0.5')
+mojs.addShape("heart", Heart);
 
-//dots
-.fromTo(dots, 6, {autoAlpha: 0}, {autoAlpha: 1, ease: Expo.easeOut}, 'flower3+=1')
-.fromTo(dots, 5, {scale: 0, transformOrigin: '50% 50%' }, {scale: 1, ease: Expo.easeOut}, 'flower3')
+const crtBoom = (delay = 0, x = 0, rd = 46) => {
+  parent = el.container;
+  const crcl = new mojs.Shape({
+    shape: "circle",
+    fill: "none",
+    stroke: el.colTxt,
+    strokeWidth: { 5: 0 },
+    radius: { [rd]: [rd + 20] },
+    easing: "quint.out",
+    duration: 500 / 3,
+    parent,
+    delay,
+    x });
+
+
+  const brst = new mojs.Burst({
+    radius: { [rd + 15]: 110 },
+    angle: "rand(60, 180)",
+    count: 3,
+    timeline: { delay },
+    parent,
+    x,
+    children: {
+      radius: [5, 3, 7],
+      fill: el.colTxt,
+      scale: { 1: 0, easing: "quad.in" },
+      pathScale: [0.8, null],
+      degreeShift: ["rand(13, 60)", null],
+      duration: 1000 / 3,
+      easing: "quint.out" } });
+
+
+
+  return [crcl, brst];
+};
+
+const crtLoveTl = () => {
+  const move = 1000;
+  const boom = 200;
+  const easing = "sin.inOut";
+  const easingBoom = "sin.in";
+  const easingOut = "sin.out";
+  const opts = { duration: move, easing, opacity: 1 };
+  const delta = 150;
+
+  return new mojs.Timeline().add([
+  new mojs.Tween({
+    duration: move,
+    onStart: () => {
+      [el.i, el.l, el.o, el.v, el.e, el.y, el.o2, el.u].forEach(el => {
+        el.style.opacity = 1;
+        el.style =
+        "transform: translate(0px, 0px) rotate(0deg) skew(0deg, 0deg) scale(1, 1); opacity: 1;";
+      });
+    },
+    onComplete: () => {
+      [el.l, el.o, el.v, el.e].forEach(el => el.style.opacity = 0);
+      el.blop.play();
+    } }),
+
+
+  new mojs.Tween({
+    duration: move * 2 + boom,
+    onComplete: () => {
+      [el.y, el.o2].forEach(el => el.style.opacity = 0);
+      el.blop.play();
+    } }),
+
+
+  new mojs.Tween({
+    duration: move * 3 + boom * 2 - delta,
+    onComplete: () => {
+      el.i.style.opacity = 0;
+      el.blop.play();
+    } }),
+
+
+  new mojs.Tween({
+    duration: move * 3 + boom * 2,
+    onComplete: () => {
+      el.u.style.opacity = 0;
+      el.blup.play();
+    } }),
+
+
+  new mojs.Tween({
+    duration: 50,
+    delay: 4050,
+    onUpdate: progress => {
+      [el.i, el.l, el.o, el.v, el.e, el.y, el.o2, el.u].forEach(el => {
+        el.style = `transform: translate(0px, 0px) rotate(0deg) skew(0deg, 0deg) scale(1, 1); opacity: ${
+        1 * progress
+        };`;
+      });
+    },
+    onComplete: () => {
+      [el.i, el.l, el.o, el.v, el.e, el.y, el.o2, el.u].forEach(el => {
+        el.style.opacity = 1;
+        el.style =
+        "transform: translate(0px, 0px) rotate(0deg) skew(0deg, 0deg) scale(1, 1); opacity: 1;";
+      });
+    } }),
+
+
+  new mojs.Html({
+    ...opts,
+    el: el.lineLeft,
+    x: { 0: 52 } }).
+
+  then({
+    duration: boom + move,
+    easing,
+    x: { to: 52 + 54 } }).
+
+  then({
+    duration: boom + move,
+    easing,
+    x: { to: 52 + 54 + 60 } }).
+
+  then({
+    duration: 150, // 3550
+    easing,
+    x: { to: 52 + 54 + 60 + 10 } }).
+
+  then({
+    duration: 300 }).
+
+  then({
+    duration: 350,
+    x: { to: 0 },
+    easing: easingOut }),
+
+
+  new mojs.Html({
+    ...opts,
+    el: el.lineRight,
+    x: { 0: -52 } }).
+
+  then({
+    duration: boom + move,
+    easing,
+    x: { to: -52 - 54 } }).
+
+  then({
+    duration: boom + move,
+    easing,
+    x: { to: -52 - 54 - 60 } }).
+
+  then({
+    duration: 150,
+    easing,
+    x: { to: -52 - 54 - 60 - 10 } }).
+
+  then({
+    duration: 300 }).
+
+  then({
+    duration: 350,
+    x: { to: 0 },
+    easing: easingOut }),
+
+
+  new mojs.Html({
+    // [I] LOVE YOU
+    ...opts,
+    el: el.i,
+    x: { 0: 34 } }).
+
+  then({
+    duration: boom,
+    easing: easingBoom,
+    x: { to: 34 + 19 } }).
+
+  then({
+    duration: move,
+    easing,
+    x: { to: 34 + 19 + 40 } }).
+
+  then({
+    duration: boom,
+    easing: easingBoom,
+    x: { to: 34 + 19 + 40 + 30 } }).
+
+  then({
+    duration: move,
+    easing,
+    x: { to: 34 + 19 + 40 + 30 + 30 } }),
+
+
+  new mojs.Html({
+    // I [L]OVE YOU
+    ...opts,
+    el: el.l,
+    x: { 0: 15 } }),
+
+
+  new mojs.Html({
+    // I L[O]VE YOU
+    ...opts,
+    el: el.o,
+    x: { 0: 11 } }),
+
+
+  new mojs.Html({
+    // I LO[V]E YOU
+    ...opts,
+    el: el.v,
+    x: { 0: 3 } }),
+
+
+  new mojs.Html({
+    // I LOV[E] YOU
+    ...opts,
+    el: el.e,
+    x: { 0: -3 } }),
+
+
+  new mojs.Html({
+    // I LOVE [Y]OU
+    ...opts,
+    el: el.y,
+    x: { 0: -20 } }).
+
+  then({
+    duration: boom,
+    easing: easingBoom,
+    x: { to: -20 - 33 } }).
+
+  then({
+    duration: move,
+    easing,
+    x: { to: -20 - 33 - 24 } }),
+
+
+  new mojs.Html({
+    // I LOVE Y[O]U
+    ...opts,
+    el: el.o2,
+    x: { 0: -27 } }).
+
+  then({
+    duration: boom,
+    easing: easingBoom,
+    x: { to: -27 - 27 } }).
+
+  then({
+    duration: move,
+    easing,
+    x: { to: -27 - 27 - 30 } }),
+
+
+  new mojs.Html({
+    // I LOVE YO[U]
+    ...opts,
+    el: el.u,
+    x: { 0: -32 } }).
+
+  then({
+    duration: boom,
+    easing: easingBoom,
+    x: { to: -32 - 21 } }).
+
+  then({
+    duration: move,
+    easing,
+    x: { to: -32 - 21 - 36 } }).
+
+  then({
+    duration: boom,
+    easing: easingBoom,
+    x: { to: -32 - 21 - 36 - 31 } }).
+
+  then({
+    duration: move,
+    easing,
+    x: { to: -32 - 21 - 36 - 31 - 27 } }),
+
+
+  new mojs.Shape({
+    parent: el.container,
+    shape: "heart",
+    delay: move,
+    fill: el.colHeart,
+    x: -64,
+    scale: { 0: 0.95, easing: easingHeart },
+    duration: 500 }).
+
+  then({
+    x: { to: -62, easing },
+    scale: { to: 0.65, easing },
+    duration: boom + move - 500 }).
+
+  then({
+    duration: boom - 50,
+    x: { to: -62 + 48 },
+    scale: { to: 0.9 },
+    easing: easingBoom }).
+
+  then({
+    duration: 125,
+    scale: { to: 0.8 },
+    easing: easingOut }).
+
+  then({
+    duration: 125,
+    scale: { to: 0.85 },
+    easing: easingOut }).
+
+  then({
+    duration: move - 200,
+    scale: { to: 0.45 },
+    easing }).
+
+  then({
+    delay: -75,
+    duration: 150,
+    x: { to: 0 },
+    scale: { to: 0.9 },
+    easing: easingBoom }).
+
+  then({
+    duration: 125,
+    scale: { to: 0.8 },
+    easing: easingOut }).
+
+  then({
+    duration: 125, // 3725
+    scale: { to: 0.85 },
+    easing: easingOut }).
+
+  then({
+    duration: 125 // 3850
+  }).
+  then({
+    duration: 350,
+    scale: { to: 0 },
+    easing: easingOut }),
+
+
+  ...crtBoom(move, -64, 46),
+  ...crtBoom(move * 2 + boom, 18, 34),
+  ...crtBoom(move * 3 + boom * 2 - delta, -64, 34),
+  ...crtBoom(move * 3 + boom * 2, 45, 34)]);
+
+};
+
+const loveTl = crtLoveTl().play();
+setInterval(() => {
+  loveTl.replay();
+}, 4300);
+
+const volume = 0.2;
+el.blup.volume = volume;
+el.blop.volume = volume;
+
+const toggleSound = () => {
+  let on = true;
+  return () => {
+    if (on) {
+      el.blup.volume = 0.0;
+      el.blop.volume = 0.0;
+      el.sound.classList.add("sound--off");
+    } else {
+      el.blup.volume = volume;
+      el.blop.volume = volume;
+      el.sound.classList.remove("sound--off");
+    }
+    on = !on;
+  };
+};
+el.sound.addEventListener("click", toggleSound());
